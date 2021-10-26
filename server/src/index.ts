e51ddcaa3782a6f795304d43a53c0774974e5a60
Constantine <constantine.ru@gmail.com>
2021-10-26 23:44:52 +0500

    * Создаем шаблон проекта - log4js, но пока без внятного конфигурирования

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,18 @@
-import express from 'express'
+import dotenv from "dotenv"
+import express from "express"
+import { configure, getLogger } from "log4js";
 
+// Загрузили конфигу в глобальную переменную process
+dotenv.config({path: "./config/.env"});
+
+// Крутим логгер
+configure("./config/log4js.json");
+// const logger = getLogger();
+
+// Стартуем express.js
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
+
 app.get('/', (request, response) => {
   response.send('Hello world!');
 });
