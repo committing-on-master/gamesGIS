7b9fc45ffded051641ae04ff55aa76bf2c3cb50e
Constantine <constantine.ru@gmail.com>
2021-10-29 11:58:20 +0500

    Регистрация - Rest API тестовый рестапи для пользователей, без бд

--- server/src/users/users.routes.config.ts
+++ server/src/users/users.routes.config.ts
@@ -1,43 +1,40 @@
-import {CommonRoutesConfig} from "../common/common.routes.config";
-
-import express, { response } from "express";
+import { CommonRoutesConfig } from '../common/common.routes.config';
+import UsersController from './controllers/users.controller';
+import UsersMiddleware from './middleware/users.middleware';
+import express from 'express';
 
 export class UsersRoutes extends CommonRoutesConfig {
     constructor(app: express.Application) {
-        super(app, "UsersRoutes");
+        super(app, 'UsersRoutes');
     }
+
     configureRoutes(): express.Application {
-        this.app.route("/users")
-            .get((request: express.Request, response: express.Response) => {
-                response.status(200).send("List of users");
-            })
-            
-            .post((request: express.Request, response: express.Response) => {
-                response.status(200).send("Post to users");
-            });
+        this.app
+            .route(`/users`)
+            .get(UsersController.listUsers)
+            .post(
+                UsersMiddleware.validateRequiredUserBodyFields,
+                UsersMiddleware.validateSameEmailDoesntExist,
+                UsersController.createUser
+            );
 
-        this.app.route("/users/:userId")
-            .all((request: express.Request, response: express.Response, next: express.NextFunction) => {
-                // this middleware function runs before any request to /users/:userId
-                // but it doesn't accomplish anything just yet---
-                // it simply passes control to the next applicable function below using next()
-                // Вот этой шляпы я не понял, как она перепрыгнет то дальше, а не внутрь.
-                // Только если некстом будет мидлваря, которая до ответов должна отработать
-                next();
-            })
-            .get((request: express.Request, response: express.Response) => {
-                response.status(200).send(`GET requested for id ${request.params.userId}`);
-            })
-            .put((request: express.Request, response: express.Response) => {
-                response.status(200).send(`PUT requested for id ${request.params.userId}`);
-            })
-            .patch((request: express.Request, response: express.Response) => {
-                response.status(200).send(`PATCH requested for id ${request.params.userId}`);
-            })
-            .delete((request: express.Request, response: express.Response) => {
-                response.status(200).send(`DELETE requested for id ${request.params.userId}`);
-            })
+        this.app.param(`userId`, UsersMiddleware.extractUserId);
+        this.app
+            .route(`/users/:userId`)
+            .all(UsersMiddleware.validateUserExists)
+            .get(UsersController.getUserById)
+            .delete(UsersController.removeUser);
 
+        this.app.put(`/users/:userId`, [
+            UsersMiddleware.validateRequiredUserBodyFields,
+            UsersMiddleware.validateSameEmailBelongToSameUser,
+            UsersController.put,
+        ]);
+
+        this.app.patch(`/users/:userId`, [
+            UsersMiddleware.validatePatchEmail,
+            UsersController.patch,
+        ]);
 
         return this.app;
     }
