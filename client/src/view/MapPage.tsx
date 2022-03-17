fd010bb3f98b8d784bd07945e8d9f8a902bd9bbe
Constantine <constantine.ru@gmail.com>
2022-03-18 01:20:07 +0500

    hoc - рефакторинг страницы с картой, замена захардкоженного спиннера

--- client/src/view/MapPage.tsx
+++ client/src/view/MapPage.tsx
@@ -1,13 +1,8 @@
 import { useParams, useNavigate } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 
-import { useAppDispatch, useAppSelector } from "../store/hooks";
+import { useAppDispatch } from "../store/hooks";
 import { useEffect, useState } from "react";
-import { AwaitingComponent } from "../components/common/AwaitingComponent";
-import { WarningComponent } from "../components/common/WarningComponent";
-
-import { selectIsEditingMode } from "../store/markers/slice";
-import { mapSelectors } from "../store/mapProfile/state";
 
 import { fetchMapProfile } from "../store/mapProfile/thunks";
 import { EventHubProvider } from "../components/maps/EventHubProvider";
@@ -16,22 +11,15 @@ import { fetchProfileMarkers } from "../store/markers/thunks";
 import { MapPanel } from "../components/panels/MapPanel";
 import { SidePanel } from "../components/panels/SidePanel";
 
-import styles from './MapPage.module.scss';
+import { ProcessState, withWaiter } from "../hocs/withWaiter";
 
-type LoadingState = {
-    status: "idle" | "loading" | "succeeded" | "failed";
-    msg?: string;
-}
+function useFetchMapProfile(): [ProcessState, string] {
+    const [text, setText] = useState('Map profile loading, please wait....');
+    const [loadingState, setLoadingState] = useState<ProcessState>(ProcessState.Loading);
 
-function MapPage() {
     const { profileName } = useParams();
     const navigate = useNavigate();
-    const [loadingState, setLoadingState] = useState<LoadingState>({ status: "idle", msg: "Profile loading, please wait." });
-
     const dispatch = useAppDispatch();
-    const mapParams = useAppSelector(state => mapSelectors.containerParams(state.map));
-    const isEditingNow = useAppSelector(state => selectIsEditingMode(state.markers));
-
 
     useEffect(() => {
         if (!profileName) {
@@ -43,44 +31,38 @@ function MapPage() {
                 dispatch(fetchProfileMarkers(profileName!))
                     .unwrap()
                     .then(() => {
-                        setLoadingState({ status: "succeeded", msg: "profile loaded" });
+                        setLoadingState(ProcessState.Succeeded);
+                        setText('profile loaded');
                     })
             })
             .catch(error => {
                 console.log(error);
-                setLoadingState({ status: "failed", msg: "profile loading failed" });
+                setLoadingState(ProcessState.Failed);
+                setText('Map profile loading failed');
             })
 
     }, [dispatch, navigate, profileName]);
 
-    if (loadingState.status === "idle" || loadingState.status === "loading") {
-        return (
-            <AwaitingComponent>
-                {loadingState.msg ?? <p>{loadingState.msg}</p>}
-            </AwaitingComponent>
-        );
-    }
+    return [loadingState, text];
+}
 
-    if (loadingState.status === "failed") {
-        return (
-            <WarningComponent>
-                {loadingState.msg ?? <p>{loadingState.msg}</p>}
-            </WarningComponent>
-        );
-    }
-
-    return (
-        <div className={styles.leafletContainer}>
+const Content = withWaiter(
+    () =>
         <EventHubProvider>
             <LeafletComponent />
             <SidePanel
                 visibility={true}
                 header="Markers"
-                >
+            >
                 <MapPanel />
             </SidePanel>
         </EventHubProvider>
-        </div>
+);
+
+function MapPage() {
+    const [state, text] = useFetchMapProfile();
+    return (
+        <Content state={state} msg={text} size={"large"} />
     );
 }
 
